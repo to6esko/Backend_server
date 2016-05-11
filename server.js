a6e4fa14b2cf2297e350810ca2d285a9fb65d4ec
Todor Krastev <to6esko69@gmail.com>
2016-05-11 19:39:31 +0300

    delete comment

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.use(function(req, res, next) {
     // an API server in conjunction with something like webpack-dev-server.
     res.setHeader('Access-Control-Allow-Origin', '*');
 
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); 
+
     // Disable caching so we'll always get the latest comments.
     res.setHeader('Cache-Control', 'no-cache');
     next();
@@ -45,12 +47,42 @@ app.get('/api/comments', function(req, res) {
   });
 });
 
+app.delete('/api/comments', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    var comments = JSON.parse(data);
+    // NOTE: In a real implementation, we would likely rely on a database or
+    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+    // treat Date.now() as unique-enough for our purposes.
+    
+    var id = req.body.id;           
+    comments = comments.filter(
+        (comment)=> {
+            return comment.id != id;
+        }
+    );       
+      
+    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      res.json(comments);
+    });
+  });
+});
+
 app.post('/api/comments', function(req, res) {
   fs.readFile(COMMENTS_FILE, function(err, data) {
     if (err) {
       console.error(err);
       process.exit(1);
     }
+
     var comments = JSON.parse(data);
     // NOTE: In a real implementation, we would likely rely on a database or
     // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
@@ -59,6 +91,7 @@ app.post('/api/comments', function(req, res) {
       id: Date.now(),
       author: req.body.author,
       text: req.body.text,
+      avatar: req.body.avatar,
     };
     comments.push(newComment);
     fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
